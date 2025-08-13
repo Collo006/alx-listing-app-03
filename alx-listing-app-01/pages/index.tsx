ae5d198feae804acda2494c039287e711f762538
Collins Kipruto <collinskipruto036@gmail.com>
2025-08-13 21:25:49 +0300

    submit

---
+++ alx-listing-app-01/pages/index.tsx
@@ -0,0 +1,13 @@
+
+const Home: React.FC = () => {
+  return (
+          <div className="bg-[url('/images/pickup truck.jpg')] bg-cover bg-center h-screen">
+
+          </div>
+    
+  )
+}
+
+export default Home;
+
+
